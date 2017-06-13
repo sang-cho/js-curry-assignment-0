d3d8a8b5950c8dc032f7a3d3492227f7df1def84
Sang Cho <Sang Cho>
2017-06-13 12:05:16 -0500

    finally finished, woot woot

--- lib.js
+++ lib.js
@@ -21,7 +21,8 @@ const cart =
  * should return an array with the `itemName` repeated `count` number of times
  */
 const itemRepeater =
-  (itemName,count) => {
+  itemName=>
+  count => {
       let arr=[]
       for (let i=0; i<count; i++){
         arr.push(itemName)
@@ -33,18 +34,24 @@ const itemRepeater =
  * should return an array of carts with each given customer's shopping list
  * as an array of items
  */
+const concat2 = (s1,s2) => s1.concat(s2)
+
+
 const constructCarts =
   listings =>
     customers => {
-       //console.log(itemRepeater(customers[0].name , 6))
       return customers.map (customer => ({
         customer: customer.name,
-        items:( entries(customer.shoppingList)
-        // .map (item =>  (itemRepeater(item[0] , item[1])))
-      )
+
+        items:( (entries(customer.shoppingList))
+        .map ( (name) =>
+        itemRepeater(name[0])(name[1])
+        )//map end
+      ) //items end
+      .reduce (concat2,[])
     })
-    )
-  }
+  )
+}
 
 module.exports = {
   listing,
